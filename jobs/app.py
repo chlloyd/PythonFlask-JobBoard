b6cad4edbace4ca6b9781a317acb58847a7efe02
Chris Lloyd <chris.lloyd2504@gmail.com>
2019-02-01 17:18:34 +0000

    Employer Reviews

--- jobs/app.py
+++ jobs/app.py
@@ -2,13 +2,12 @@ import sqlite3
 import datetime
 from flask import Flask, render_template, g, request, redirect, url_for
 
-PATH = "db/jobs.sqlite"
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
-
 def open_connection():
-    connection = getattr(g, "_connection", None)
+    connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
@@ -21,6 +20,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+
     cursor.close()
     return results
 
@@ -30,13 +30,11 @@ def close_connection(exception):
     if connection is not None:
         connection.close()
 
-
 @app.route('/')
 @app.route('/jobs')
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
-
 
 @app.route('/job/<job_id>')
 def job(job_id):
@@ -50,9 +48,8 @@ def employer(employer_id):
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
 
-
 @app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
-def review(employee_id):
+def review(employer_id):
     if request.method == 'POST':
         review = request.form['review']
         rating = request.form['rating']
